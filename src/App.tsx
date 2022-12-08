deff6db6b2839816936ea75557421ed751ef35c3
Hugo Silva <hugo.marcelo@clevertech.biz>
2022-12-08 13:56:37 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+
+import { Router } from './Router';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+import { CycleContextProvider } from './contexts/CyclesContext';
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <CycleContextProvider>
+          <Router />
+        </CycleContextProvider>
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
